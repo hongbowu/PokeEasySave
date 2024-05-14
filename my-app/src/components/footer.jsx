d5f39e36bf8cf4a116a9c4240f44de7fee091897
hongbowu <wuhongbo87@gmail.com>
2024-05-14 11:17:41 -0400

    add footer component

---
+++ my-app/src/components/footer.jsx
@@ -0,0 +1,22 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+
+export default function Footer() {
+  
+    const location = useLocation();
+    const navigate = useNavigate();
+    return (
+      <footer className="w-100 mt-auto text-dark p-4">
+        <div className="container text-center mb-5">
+          {location.pathname !== '/' && (
+            <button
+              className="btn btn-dark mb-3"
+              onClick={() => navigate(-1)}
+            >
+              &larr; Go Back
+            </button>
+          )}
+          <h4>&copy; {new Date().getFullYear()} - Easy-Find</h4>
+        </div>
+      </footer>
+    );
+}
