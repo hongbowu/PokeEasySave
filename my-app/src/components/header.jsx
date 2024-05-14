e1b98bf0671608f7a12277bc9b71e6bb1499cee8
hongbowu <wuhongbo87@gmail.com>
2024-05-14 13:34:51 -0400

    change header title

--- my-app/src/components/header.jsx
+++ my-app/src/components/header.jsx
@@ -2,7 +2,7 @@ export default function Header () {
 
     return (
         <header>
-        <h1>I am a header!</h1>
+        <h1>Poke Easy Save!</h1>
         <nav>
             <div>login</div>
             <div>sign up</div>
