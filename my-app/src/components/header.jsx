4a361760d7fac8e0573421cd13df484fbc92cfca
hongbowu <wuhongbo87@gmail.com>
2024-05-14 11:17:06 -0400

    add header component

---
+++ my-app/src/components/header.jsx
@@ -0,0 +1,12 @@
+export default function Header () {
+
+    return (
+        <header>
+        <h1>I am a header!</h1>
+        <nav>
+            <div>login</div>
+            <div>sign up</div>
+        </nav>
+        </header>
+    )
+}
